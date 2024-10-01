50e9fed5ccbbfdf682a4bc8586c94b119212ec2e
Miguel <miguelgonalves@hotmail.com>
2024-10-01 19:53:57 -0300

    Add files via upload

---
+++ static/js/perguntas.js
@@ -0,0 +1,25 @@
+const palavras = [
+    "educação financeira",
+    "orçamentário",
+    "controle de gastos",
+    "reserva",
+    "planilha",
+    "análise prévia",
+    "endividamento",
+    "desnecessários",
+    "orçamento",
+    "segurança",
+];
+
+const dicas = [
+    "Aprender a gerenciar dinheiro.",
+    "Controle de receitas e despesas.",
+    "Acompanhe o que você gasta.",
+    "Um colchão financeiro.",
+    "Pode ser digital ou de papel.",
+    "Pense antes de comprar.",
+    "Pode levar a problemas financeiros.",
+    "Pergunte-se se é essencial.",
+    "Verifique suas finanças frequentemente.",
+    "Tranquilidade em relação ao dinheiro.",
+];
